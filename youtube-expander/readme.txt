79f1e07770f1690e2cdf535c6c40d18c41d4b7e9
Kristian Olsson <git@kasa.nu>
2011-02-25 21:19:28 +0100

    added youtube expander

---
+++ youtube-expander/readme.txt
@@ -0,0 +1,27 @@
+=== Youtube Expander ===
+Contributors: Kristian Olsson
+Donate link: http://www.kasa.nu/
+Tags: youtube, expander
+Tested up to: 3.1
+Requires at least: 3.0
+Stable tag: trunk
+
+Scans posts for Youtube URLs and expands them to show a playable video at the end of the post. 
+
+== Description ==
+
+This plugin will scan all posts for youtube.com/watch\?v=xxx links and add a video player 
+to the end of the post to play the clip.
+
+== Installation ==
+
+To install the Youtube Expander:
+
+1. Upload 'youtube-expander.php' to '/wp-content/plugins/youtube-expander/'.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Include youtube links in your posts 
+
+== Changelog ==
+
+= 1.0 =
+* First version.
---
+++ youtube-expander/youtube-expander.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Plugin Name: Youtube Expander
+Plugin URI: http://www.kasa.nu
+Description: Scans posts for Youtube URLs and expands them to show a playable video at the end of the post.  
+Version: 1.0
+Author: Kristian Olsson
+Author URI: http://www.kasa.nu
+*/
+?>
+<?php
+function showyoutube($text){
+  $foo = $text;
+  $tmp = strip_tags($text);
+  $width = 425; $height = 344;		// Standart Screen Size
+  $widthwd = 560; $heightwd = 340;	// Widescreen Size
+
+  // find a mention of youtube  youtube.com/watch?v=A1zySeNpW20
+  if (preg_match_all('#youtube.com/watch\?v=([\d\w]+)#', $tmp, $matches, PREG_PATTERN_ORDER) > 0) {
+    foreach ($matches[1] as $match) {
+ 		$images .= '<object width="'.$widthwd.'" height="'.$heightwd.'" allowfullscreen="true" type="application/x-shockwave-flash" data="http://www.youtube.com/v/'.$match.'"><param name="movie" value="http://www.youtube.com/v/'.$match.'" /></object>';
+   //  $images .= '<object type="application/x-shockwave-flash" width="'.$width.'" height="'.$height.'" data="http://www.youtube.com/v/'.$match.'"><param name="movie" value="http://www.youtube.com/v/'.$match.'"></param><param name="allowFullScreen" value="true"></param><param name="wmode" value="transparent" /></object>';
+//$images .= "<a href='http://youtube.com/{$match}'><img src='http://youtube.com/show/large/{$match}' class='aligncenter' /></a>";
+    }
+    $text = $foo . '<center>' . $images . '</center>';
+  }
+
+  return $text;
+}
+
+add_filter('the_content', 'showyoutube');
+add_filter('the_excerpt', 'showyoutube');
+?>
